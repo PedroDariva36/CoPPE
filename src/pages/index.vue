78c422c85f87cb47f164c77672c46edbefd2e6d4
PedroDariva36 <darivafilho@gmail.com>
2024-11-19 23:13:22 -0300

    Adding better tag system

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,65 +1,76 @@
 <template>
-  <v-container class = "bg-variant">
-    <v-container class="" fluid>
-      <v-responsive class="mx-auto text-left">
-        <p class="font-weight-medium text-primary">Competitive Programing Problem Enumerator</p>
-        <p class="font-weight-bold text-sm-h2 text-h4 mt-2'"> CoPPE</p>
-      </v-responsive>
-    </v-container>
-    <v-card flat >
-      <template v-slot:text>
-        <v-text-field
-            v-model="search"
-            label="Search"
-            prepend-inner-icon="mdi-magnify"
-            variant="outlined"
-            hide-details
-            single-line
-            ></v-text-field>
+  <v-container class = "bg-variant"> <!-- d-flex flex-column fill-height justify-center align-center " > -->
+    <v-app-bar :elevation="0">
+      <v-app-bar-title>
+      <!--
+          <p class="font-weight-bold text-sm-h2 text-h4 mt-2'"> CoPPE</p> <span class="font-weight-medium text-primary">Competitive Programing Problem Enumerator</span> --> CoPPE </v-app-bar-title>
+      <template v-slot:append>
+        <v-btn icon @click="dialog = true"> <v-icon>mdi-plus</v-icon> </v-btn>
+        <v-btn icon @click="dl()"> <v-icon>mdi-export</v-icon> </v-btn>
       </template>
+    </v-app-bar>
 
-      <v-data-table
-          :headers="headers"
-          :items="problems"
-          :search="search"
-          >
+    <!-- -->
+      <v-card flat>
+        <template v-slot:text>
+          <v-text-field
+              v-model="search"
+              label="Search"
+              prepend-inner-icon="mdi-magnify"
+              variant="outlined"
+              hide-details
+              single-line
+          ></v-text-field>
+        </template>
 
-          <template #item.tags="{ item }">
-            <v-chip-group>
-              <template v-for="i in item.tags" :key="i">
-                <v-chip> {{ i }} </v-chip>
-              </template>
-            </v-chip-group>
+        <v-data-table :headers="headers" :items="problems" :search="search">
+          <template #item.local= "{item}">
+            <a target="_blank" :href="item.link"> {{ item.local }} </a>
           </template>
 
-      </v-data-table>
-      <v-container>
-        <v-btn icon @click="dialog = true"> <v-icon>mdi-plus</v-icon> </v-btn>
+          <template #item.tags="{ item }" >
+            <v-row>
+              <v-cols cols= "3" >
+                  <template v-for="i in item.tags" :key="i">
+                    <v-chip :color ="getColorTag(i)"> {{ i }} </v-chip>
+                  </template>
 
-        <v-dialog v-model="dialog" width="auto">
-          <v-card
-              max-width="400"
-              prepend-icon="mdi-update"
-              text="Your application will relaunch automatically after the update is complete."
-              title="Update in progress"
-              >
-              <template v-slot:actions>
-                <v-btn
-                    class="ms-auto"
-                    text="Ok"
-                    @click="dialog = false"
-                    ></v-btn>
-              </template>
-          </v-card>
-        </v-dialog>
+                  <div class="d-flex justify-center ga-2 overflow-y " max-width = '200'>
+                  </div>
+              </v-cols>
+            </v-row>
+          </template>
 
-        <v-btn icon @click="dl()">
-          <v-icon>mdi-export</v-icon>
-        </v-btn>
-      </v-container>
-    </v-card>
+          <template #item.dificulty = "{ item }">
+            <v-chip label :color = "getColorDif(item.dificulty)" >
+              {{ item.dificulty }}
+            </v-chip>
+          </template>
+
+        </v-data-table>
+      </v-card>
+
+
+      <!-- MODAL -->
+    <v-dialog v-model="dialog" width="auto">
+      <v-card
+          max-width="400"
+          prepend-icon="mdi-update"
+          text="Your application will relaunch automatically after the update is complete."
+          title="Update in progress"
+          >
+          <template v-slot:actions>
+            <v-btn
+                class="ms-auto"
+                text="Ok"
+                @click="dialog = false"
+                ></v-btn>
+          </template>
+      </v-card>
+    </v-dialog>
 
   </v-container>
+
 </template>
 <script setup lang="ts">
   import { problems } from "../problems.js";
@@ -68,29 +79,68 @@
   const dialog = ref(false);
   const search = ref ('');
   const headers = ref([
-      {
-        align: 'start',
-        key: 'code',
-        title: 'Code',
-      },
+      { align: 'start', key: 'code', title: 'Code'},
+      { key: 'name', title: 'Name'},
       { key: 'local', title: 'From' },
       { key: 'tags', title: 'Tags' },
+      { key: 'dificulty', title: 'Dificulty' },
   ])
+  const tags = new Map();
+  tags.set('Dynamic Programming', 'deep-orange');
+
+  tags.set('Brute Force', 'deep-orange');
+
+  tags.set('Graphs', 'purple-accent-1');
+
+  tags.set('Data Structures', 'purple-accent-1');
+
+  tags.set('Number Theory', 'cyan-accent-1');
+
+  tags.set('Math', 'yellow');
+
+  tags.set('MEX', 'yellow-accent-2');
+  tags.set('Permutation', 'yellow-accent-2');
+  tags.set('Matrices', 'yellow-accent-2');
 
 
 
-function dl(){
-    const filename = 'problems.js';
-    const jsonStr = JSON.stringify(problems);
-    let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
-    element.setAttribute('download', filename);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-}
+  tags.set('Geometry', 'amber');
+
+  tags.set('Binary Search', 'blue-accent-3');
+
+  tags.set('Bitmasks', 'light-blue');
+
+  tags.set('Strings', 'indigo-accent-1');
+
+  tags.set('Greedy', 'green-accent-4');
+
+  tags.set('Two Pointers', 'lime-accent-3');
+
+  tags.set('Sorting', 'light-green-accent-4');
 
 
+  function getColorDif(rating) {
+      if (rating < 1200) return 'gray'
+      else if (rating < 1400) return 'green'
+      else if (rating < 1600) return 'cyan'
+      else if (rating < 1900) return 'blue'
+      else if (rating < 2400) return 'yellow'
+      else return 'red'
+  }
 
+  function getColorTag(tag) {
+      return tags.get(tag);
+  }
+
+  function dl(){
+      const filename = 'problems.js';
+      const jsonStr = JSON.stringify(problems);
+      let element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonStr));
+      element.setAttribute('download', filename);
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+  }
 </script>
--- src/problems.js
+++ src/problems.js
@@ -1,13 +1,80 @@
 export const problems = [
   {
     code: "590B",
+    name: "Painting Pebbles",
     local: "Codeforces",
-    tags: ["Binary Search", "Geometry", "Math"]
+    tags: ["Binary Search", "Geometry", "Math"],
+    link: "https://codeforces.com/problemset/problem/509/B",
+    dificulty: 1300
   },
   {
+    code: "2037D",
+    name: "Sharky Surfing",
+    local: "Codeforces",
+    tags: ["Greedy"],
+    link: "https://codeforces.com/contest/2037/problem/D",
+    dificulty: 1300
+  },
+  {
+    code: "2037C",
+    name: "Superultra's Favorite Permutation",
+    local: "Codeforces",
+    tags: ["Greedy", "Permutation", "Number Theory"],
+    link: "https://codeforces.com/contest/2037/problem/C",
+    dificulty: 1000
+  },
+  {
+    code: "2036E",
+    name: "Reverse the Rivers",
+    local: "Codeforces",
+    tags: ["Binary Search", "Greedy"],
+    link: "https://codeforces.com/contest/2036/problem/E",
+    dificulty: 1600
+  },
+  {
+    code: "2036D",
+    name: "I Love 1543",
+    local: "Codeforces",
+    tags: ["Brute Force", "Matrices"],
+    link: "https://codeforces.com/contest/2036/problem/D",
+    dificulty: 1300
+  },
+  {
+    code: "2036C",
+    name: "Anya and 1100",
+    local: "Codeforces",
+    tags: ["Brute Force"],
+    link: "https://codeforces.com/contest/2036/problem/C",
+    dificulty: 1100
+  },
+  {
+    code: "2031D",
+    name: "Penchick and Desert Rabbit",
+    local: "Codeforces",
+    tags: ["Data Structures"],
+    link: "https://codeforces.com/contest/2031/problem/D",
+    dificulty: 1700
+  },
+  {
+    code: "2031C",
+    name: "Penchick and BBQ Buns",
+    local: "Codeforces",
+    tags: ["Greedy", "Math"],
+    link: "https://codeforces.com/contest/2031/problem/C",
+    dificulty: 1300
+  },
+  {
+    code: "2031B",
+    name: "Penchick and Satay Sticks",
+    local: "Codeforces",
+    tags: ["Permutation"],
+    link: "https://codeforces.com/contest/2031/problem/B",
+    dificulty: 900
+  }
+  ,{
     code: "Test",
-    local: "Uva",
-    tags: ["DP","Number Theory"]
+    local: "Test",
+    tags: ["Dynamic Programming","Number Theory", "Greedy", "Sorting", "Graphs", "Bitmasks", "Brute Force", "Strings", "Two Pointers", "MEX", "Permutation"]
   }
 ]
 
