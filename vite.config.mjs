96301ba68a06606747cb47dfa3cb3dc8179e600b
Pedro Dariva <47556893+PedroDariva36@users.noreply.github.com>
2024-12-03 05:33:19 -0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CoPPE/',
+  //base: '/CoPPE/',
   plugins: [
     VueRouter(),
     Vue({
